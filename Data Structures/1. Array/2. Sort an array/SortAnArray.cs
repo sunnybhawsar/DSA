6d8a536ab630b4753098df4a6378e45695c98a5f
Sunny Bhawsar <36097146+sunnybhawsar@users.noreply.github.com>
2022-05-30 11:10:03 +0530

    Update SortAnArray.cs

--- Data Structures/1. Array/2. Sort an array/SortAnArray.cs
+++ Data Structures/1. Array/2. Sort an array/SortAnArray.cs
@@ -3,12 +3,24 @@
 
 using System;
 
-public class Solution
-{	
-	public static void Main(string[] args)
-	{
-		
-	}
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = new int[] {2, 4, 1, 7, 9, 3, 5};
+
+        Array.Sort(arr);
+
+        PrintArray(arr);
+    }
+
+    private static void PrintArray(int[] arr)
+    {
+        Array.ForEach<int>(arr, a => Console.Write(a + " "));
+    }
 }
 
 // Sunny Bhawsar
+
+// Using Sort function of Array class
+// Complexity: O(n log n)
